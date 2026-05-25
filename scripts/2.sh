d8ca07952a3f7e1e919e42dc33e3b5fb609d40ee
zhanghw23 <hanwenzhang23@gmail.com>
2026-05-25 20:21:05 +0800

    remove experiment scripts

--- scripts/2.sh
+++
@@ -1,2 +0,0 @@
-CUDA_VISIBLE_DEVICES=5 python src/encode_doc.py --model_name llama3.2-1b-instruct --dataset 2wikimultihopqa --task_type open_domain_qa --with_cot --block_size 1500
-CUDA_VISIBLE_DEVICES=5 python src/encode_doc.py --model_name llama3.2-1b-instruct --dataset hotpotqa --task_type open_domain_qa --with_cot --block_size 1500
